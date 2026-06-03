5f051170fbf5364200acbc24647591ba590d6cb8
Leandro Pinheiro <leo@mentesnotaveis.com.br>
2026-06-03 19:31:47 -0300

    stage 1 finished

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,76 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+	// Área para definição das variáveis para armazenar as propriedades das cidades
+	char state;
+	char code[10];
+	char cityName[50];
+	int population;
+	float area;
+	float pib;
+	int nTouristAttractions;
 
-  // Área para entrada de dados
+	char state2;
+	char code2[10];
+	char cityName2[50];
+	int population2;
+	float area2;
+	float pib2;
+	int nTouristAttractions2;
 
-  // Área para exibição dos dados da cidade
+	// Área para entrada de dados
+	printf("Digite as informações da primeira carta:\n");
 
-return 0;
+	printf("Estado: ");
+	scanf(" %c", &state);
+	printf("Código: ");
+	scanf("%s", code);
+	printf("Nome da Cidade: ");
+	scanf(" %[^\n]", cityName);
+	printf("População: ");
+	scanf("%d", &population);
+	printf("Área (Km²): ");
+	scanf("%f", &area);
+	printf("PIB: ");
+	scanf("%f", &pib);
+	printf("Número de Pontos Turísticos: ");
+	scanf("%d", &nTouristAttractions);
+
+	printf("\nDigite as informações da segunda carta:\n");
+
+	printf("Estado: ");
+	scanf(" %c", &state2);
+	printf("Código: ");
+	scanf("%s", code2);
+	printf("Nome da Cidade: ");
+	scanf(" %[^\n]", cityName2);
+	printf("População: ");
+	scanf("%d", &population2);
+	printf("Área (Km²): ");
+	scanf("%f", &area2);
+	printf("PIB: ");
+	scanf("%f", &pib2);
+	printf("Número de Pontos Turísticos: ");
+	scanf("%d", &nTouristAttractions2);
+
+	// Área para exibição dos dados
+	printf("\nPrimeira Carta:\n");
+	printf("Estado: %c\n", state);
+	printf("Código: %s\n", code);
+	printf("Nome da Cidade: %s\n", cityName);
+	printf("População: %d\n", population);
+	printf("Área (Km²): %.2f\n", area);
+	printf("PIB: %.2f\n", pib);
+	printf("Número de Pontos Turísticos: %d\n", nTouristAttractions);
+
+	printf("\nSegunda Carta:\n");
+	printf("Estado: %c\n", state2);
+	printf("Código: %s\n", code2);
+	printf("Nome da Cidade: %s\n", cityName2);
+	printf("População: %d\n", population2);
+	printf("Área (Km²): %.2f\n", area2);
+	printf("PIB: %.2f\n", pib2);
+	printf("Número de Pontos Turísticos: %d\n", nTouristAttractions2);
+
+	return 0;
 } 
